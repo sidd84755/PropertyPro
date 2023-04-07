b7b274a550c36d9e8cb64fdf7ee30490720f57cf
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-04-07 20:00:42 +0530

    worked on front-end

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import { useList } from '@pankod/refine-core';
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components'
 
 const home = () => {
   return (
-    <div>
-      home
-    </div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart
+          title="Properties for Sale"
+          value={684}
+          series={[75, 25]}
+          colors={['#475ae8', '#e4b8ef']}
+        />
+        <PieChart
+          title="Properties for Rent"
+          value={550}
+          series={[60, 40]}
+          colors={['#475ae8', '#e4b8ef']}
+        />
+        <PieChart
+          title="Total customers"
+          value={5684}
+          series={[75, 25]}
+          colors={['#475ae8', '#e4b8ef']}
+        />
+        <PieChart
+          title="Total Cities"
+          value={555}
+          series={[75, 25]}
+          colors={['#475ae8', '#e4b8ef']}
+        />
+      </Box>
+      <Stack mt="25px" width="100%" direction={{ xs:'column', lg:'row' }}>
+        <TotalRevenue/>
+        <PropertyReferrals/>
+      </Stack>
+    </Box>
   )
 }
 
