ad8b68a4889cc205a4391944caff51e19c94d72e
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-04-08 12:53:01 +0530

    worked on forms

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { Box, Typography, FormControl, FormHelperText, TextField, 
+  TextareaAutosize, Stack, Select, MenuItem, Button } from '@pankod/refine-mui';
+import { FormProps } from 'interfaces/common';
+import CustomButton from './CustomButton';
 
-const Form = () => {
+const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, 
+onFinishHandler, propertyImage}: FormProps) => {
   return (
-    <div>
-      Form
-    </div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        {type} a Property
+      </Typography>
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fcfcfc">
+        <form style={{ marginTop:'20px', width:'100%', display:'flex', 
+        flexDirection:'column', gap:'20px'}} onSubmit={handleSubmit(onFinishHandler)}>
+
+        </form>
+      </Box>
+    </Box>
   )
 }
 
