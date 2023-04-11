11f41c6a87caba1bbc88bc225f56f9556373a153
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-04-11 14:24:15 +0530

    worked on CRUD functionality

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -3,6 +3,7 @@ import { useTable } from '@pankod/refine-core';
 import { Box, Stack, Typography, TextField, Select, MenuItem } from '@pankod/refine-mui';
 import { useNavigate } from "@pankod/refine-react-router-v6";
 import { PropertyCard, CustomButton } from "components";
+import { useMemo } from "react";
 
 const AllProperties = () => {
   const navigate = useNavigate();
@@ -18,6 +19,16 @@ const AllProperties = () => {
   const toggleSort = (field: string) => {
     setSorter([{ field, order: currentPrice === 'asc' ? 'desc' : 'asc'}])
   }
+
+  const currentFilterValues = useMemo(() => {
+    const logicalFilters = filters.flatMap((item) => 
+    ('field' in item ? item : []));
+
+    return{
+      title: logicalFilters.find((item) => item.field === 'title')?.value || '',
+      propertyType: logicalFilters.find((item) => item.field === 'propertyType')?.value || '',
+    }
+  }, [filters])
 
   if(isLoading) return <Typography>Loading...</Typography>
   if(isError) return <Typography>Error...</Typography>
@@ -41,8 +52,16 @@ const AllProperties = () => {
               variant="outlined"
               color="info"
               placeholder="Search by title"
-              value=''
-              onChange={() => {}}            
+              value={currentFilterValues.title}
+              onChange={(e) => {
+                setFilters([
+                  {
+                    field: 'title',
+                    operator: 'contains',
+                    value: e.currentTarget.value ? e.currentTarget.value : undefined
+                  }
+                ])
+              }}            
             />
             <Select
               variant="outlined"
@@ -51,10 +70,22 @@ const AllProperties = () => {
               required
               inputProps={{ 'aria-label': 'Without label' }}
               defaultValue=""
-              value=""
-              onChange={() => {}}
+              value={currentFilterValues.propertyType}
+              onChange={(e) => {
+                setFilters([
+                  {
+                    field: 'propertyType',
+                    operator: 'eq',
+                    value: e.target.value
+                  }
+                ], 'replace')
+              }} 
             >
               <MenuItem value="">All</MenuItem>
+              {['Apartment', 'Villa', 'Farmhouse', 'Condos', 'Townhouse', 'Duplex', 'Studio', 'Chalet']
+              .map((type) => (
+                <MenuItem key={type} value={type.toLowerCase()}>{type}</MenuItem>
+              ))}
             </Select>
           </Box>
         </Box>
@@ -107,7 +138,7 @@ const AllProperties = () => {
             required
             inputProps={{ 'aria-label': 'Without label' }}
             defaultValue={10}
-            onChange={() => {}}
+            onChange={(e) => setPageSize(e.target.value ? Number(e.target.value) : 10)}
           >
             {[10, 20, 30, 40, 50].map((size) => (
             <MenuItem key={size} value={size}>Show {size}</MenuItem>
--- client/src/pages/property-details.tsx
+++ client/src/pages/property-details.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+import { useDelete, useGetIdentity, useShow } from "@pankod/refine-core";
+import { useParams, useNavigate } from "@pankod/refine-react-router-v6";
+import { ChatBubble, Delete, Edit, Phone, Place, Star } from "@mui/icons-material";
 
-const propertyDetails = () => {
+const PropertyDetails = () => {
+
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const { id } = useParams();
+  const { mutate } = useDelete();
+  const { queryResult } = useShow();
+  const { data, isLoading, isError } = queryResult;
+  const propertyDetails = data?.data ?? {};
+
+  if(isLoading) return <div>Loading...</div>
+  if(isError) return <div>Error</div>
+
   return (
-    <div>
-      propertyDetails
-    </div>
+    <Box 
+      borderRadius="15px"
+      padding="20px"
+      bgcolor="#fcfcfc"
+      width="fit-content"
+    >
+      <Typography fontSize={25} fontWeight={700} color="#11142d">Details</Typography>
+    </Box>
   )
 }
 
-export default propertyDetails
+export default PropertyDetails
